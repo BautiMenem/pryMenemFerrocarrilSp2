c5489a67449eaad50c7b1bfca20e7c5740bdc770
root <mgiraudo@ies21.edu.ar>
2022-10-26 12:26:37 -0300

    Ferro

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -29,37 +29,32 @@
         private void InitializeComponent()
         {
             this.lblDias = new System.Windows.Forms.Label();
-            this.nudDias = new System.Windows.Forms.NumericUpDown();
             this.lblDistancia = new System.Windows.Forms.Label();
             this.txtDistancia = new System.Windows.Forms.TextBox();
             this.mrcCosto = new System.Windows.Forms.GroupBox();
             this.lblKm = new System.Windows.Forms.Label();
             this.lblPrecio = new System.Windows.Forms.Label();
             this.lblTotal = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).BeginInit();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.TxtTotal = new System.Windows.Forms.TextBox();
+            this.nudDias = new System.Windows.Forms.NumericUpDown();
             this.mrcCosto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).BeginInit();
             this.SuspendLayout();
             // 
             // lblDias
             // 
             this.lblDias.AutoSize = true;
-            this.lblDias.Location = new System.Drawing.Point(137, 84);
+            this.lblDias.Location = new System.Drawing.Point(90, 58);
             this.lblDias.Name = "lblDias";
             this.lblDias.Size = new System.Drawing.Size(30, 13);
             this.lblDias.TabIndex = 0;
             this.lblDias.Text = "Días";
             // 
-            // nudDias
-            // 
-            this.nudDias.Location = new System.Drawing.Point(213, 82);
-            this.nudDias.Name = "nudDias";
-            this.nudDias.Size = new System.Drawing.Size(74, 20);
-            this.nudDias.TabIndex = 1;
-            // 
             // lblDistancia
             // 
             this.lblDistancia.AutoSize = true;
-            this.lblDistancia.Location = new System.Drawing.Point(137, 126);
+            this.lblDistancia.Location = new System.Drawing.Point(90, 104);
             this.lblDistancia.Name = "lblDistancia";
             this.lblDistancia.Size = new System.Drawing.Size(51, 13);
             this.lblDistancia.TabIndex = 2;
@@ -67,16 +62,20 @@
             // 
             // txtDistancia
             // 
-            this.txtDistancia.Location = new System.Drawing.Point(213, 126);
+            this.txtDistancia.Location = new System.Drawing.Point(166, 101);
             this.txtDistancia.Name = "txtDistancia";
             this.txtDistancia.Size = new System.Drawing.Size(74, 20);
             this.txtDistancia.TabIndex = 3;
+            this.txtDistancia.TextChanged += new System.EventHandler(this.txtDistancia_TextChanged);
+            this.txtDistancia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDistancia_KeyPress);
             // 
             // mrcCosto
             // 
+            this.mrcCosto.Controls.Add(this.TxtTotal);
+            this.mrcCosto.Controls.Add(this.txtPrecio);
             this.mrcCosto.Controls.Add(this.lblTotal);
             this.mrcCosto.Controls.Add(this.lblPrecio);
-            this.mrcCosto.Location = new System.Drawing.Point(117, 179);
+            this.mrcCosto.Location = new System.Drawing.Point(67, 159);
             this.mrcCosto.Name = "mrcCosto";
             this.mrcCosto.Size = new System.Drawing.Size(217, 100);
             this.mrcCosto.TabIndex = 4;
@@ -86,7 +85,7 @@
             // lblKm
             // 
             this.lblKm.AutoSize = true;
-            this.lblKm.Location = new System.Drawing.Point(293, 129);
+            this.lblKm.Location = new System.Drawing.Point(246, 104);
             this.lblKm.Name = "lblKm";
             this.lblKm.Size = new System.Drawing.Size(22, 13);
             this.lblKm.TabIndex = 5;
@@ -105,28 +104,69 @@
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(17, 64);
+            this.lblTotal.Location = new System.Drawing.Point(14, 68);
             this.lblTotal.Name = "lblTotal";
             this.lblTotal.Size = new System.Drawing.Size(31, 13);
             this.lblTotal.TabIndex = 1;
             this.lblTotal.Text = "Total";
             // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(99, 31);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(74, 20);
+            this.txtPrecio.TabIndex = 2;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.txtPrecio_TextChanged);
+            // 
+            // TxtTotal
+            // 
+            this.TxtTotal.Location = new System.Drawing.Point(99, 61);
+            this.TxtTotal.Name = "TxtTotal";
+            this.TxtTotal.Size = new System.Drawing.Size(74, 20);
+            this.TxtTotal.TabIndex = 3;
+            // 
+            // nudDias
+            // 
+            this.nudDias.Location = new System.Drawing.Point(166, 58);
+            this.nudDias.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudDias.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDias.Name = "nudDias";
+            this.nudDias.Size = new System.Drawing.Size(74, 20);
+            this.nudDias.TabIndex = 6;
+            this.nudDias.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDias.ValueChanged += new System.EventHandler(this.nudDias_ValueChanged);
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 450);
+            this.ClientSize = new System.Drawing.Size(351, 306);
+            this.Controls.Add(this.nudDias);
             this.Controls.Add(this.lblKm);
             this.Controls.Add(this.mrcCosto);
             this.Controls.Add(this.txtDistancia);
             this.Controls.Add(this.lblDistancia);
-            this.Controls.Add(this.nudDias);
             this.Controls.Add(this.lblDias);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmMain";
             this.Text = "Ferrocarril - Calculo";
-            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).EndInit();
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.mrcCosto.ResumeLayout(false);
             this.mrcCosto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -135,13 +175,15 @@
         #endregion
 
         private System.Windows.Forms.Label lblDias;
-        private System.Windows.Forms.NumericUpDown nudDias;
         private System.Windows.Forms.Label lblDistancia;
         private System.Windows.Forms.TextBox txtDistancia;
         private System.Windows.Forms.GroupBox mrcCosto;
         private System.Windows.Forms.Label lblKm;
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox TxtTotal;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.NumericUpDown nudDias;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -12,12 +12,49 @@ namespace pryMenemFerrocarrilSp2
 {
     public partial class frmMain : Form
     {
+         public int Distancia;
+         public int Dias;
+         public float Precio;
+         public float Total;
+         
+        
         public frmMain()
         {
             InitializeComponent();
         }
 
         private void lblKm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtPrecio_TextChanged(object sender, EventArgs e)
+        {
+            Precio = Convert.ToInt32(txtDistancia.Text) * 5;
+
+            txtPrecio.Text = Precio.ToString();
+
+        }
+
+        private void txtDistancia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true; 
+            }
+        }
+
+        private void nudDias_ValueChanged(object sender, EventArgs e)
+        {
+            Dias = Convert.ToInt32(nudDias.Value);
+        }
+
+        private void txtDistancia_TextChanged(object sender, EventArgs e)
+        {
+            Distancia = Convert.ToInt32(txtDistancia.Text);
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
         {
 
         }
